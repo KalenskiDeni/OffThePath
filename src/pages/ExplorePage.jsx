007b90d8528de2dacb961efa3b99f6c05280888f
betaja <144784304+betaja@users.noreply.github.com>
2024-10-31 11:03:21 +0100

    dont bother

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import '/src/styles/explore.css';
+import React from "react";
+import "/src/styles/explore.css";
 
 export default function ExplorePage() {
   return (
     <section className="page">
-        <div className="container">
-      <h2>Explore Page</h2>
+      <div className="container">
+        <h2>Explore Page</h2>
       </div>
     </section>
   );
