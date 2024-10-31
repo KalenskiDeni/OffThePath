b5f20bd9019d58ce02ff23eee3b7f649198fcb84
betaja <144784304+betaja@users.noreply.github.com>
2024-10-31 13:45:31 +0100

    images are showing in exploore yeyyy

--- src/components/ExploreCard.jsx
+++ src/components/ExploreCard.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 export default function ExploreCard({ post }) {
-  const navigate = useNavigate();
+  // const navigate = useNavigate(); - (this is the old code from PostCard.jsx, needs to be activated when i figure out line 7)
 
   function handleNavigate() {
     //add a place to where the explore card leads to - navigate(`/posts/${post.id}`); - (this is the old code from PostCard.jsx)
@@ -9,7 +9,7 @@ export default function ExploreCard({ post }) {
 
   return (
     <article className="explore-card" onClick={handleNavigate}>
-      <img src={post.image} alt={post.location} />
+      <img src={post.photo} alt={post.location} />
       <h6>{post.location}</h6>
     </article>
   );
