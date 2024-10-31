4a1b776710311b8658ef17b5e3cc31300387f076
betaja <144784304+betaja@users.noreply.github.com>
2024-10-31 11:18:31 +0100

    json changes and explore page changes

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 import "/src/styles/explore.css";
 
 export default function ExplorePage() {
+  const [posts, setPosts] = useState([]); // set the initial state to an empty array
+
+  // Fetch data from the API
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch(
+        "https://fb-rest-race-default-rtdb.firebaseio.com/posts.json"
+      ); // fetch data from the url
+      const data = await response.json(); // get the data from the response and parse it
+      // from object to array
+      const postsArray = Object.keys(data).map((postId) => ({
+        id: postId,
+        ...data[postId],
+      })); // map the data to an array of objects
+
+      setPosts(postsArray); // set the posts state with the postsArray
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className="page">
-      <div className="container">
-        <h2>Explore Page</h2>
+      <div className="grid">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
       </div>
     </section>
   );
