6617ed0740d426335a7113ab81082687c5486c20
KalenskiDeni <kallenski.deni@gmail.com>
2024-11-01 19:00:04 +0100

    post updated

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import UserAvatar from "./UserAvatar";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import heartIcon from '../assets/icons/heart.svg';
+import commentIcon from '../assets/icons/comment.svg';
+import messageIcon from '../assets/icons/paper-plane.svg';
 
 
 export default function PostCard({ post }) {
@@ -14,18 +16,35 @@ export default function PostCard({ post }) {
   return (
     <article className="post-card" onClick={handleNavigate}>
       <UserAvatar uid={post.uid} />
-      <h6>{post.location}</h6>
-      <h6>{post.time}</h6>
-      <p>{post.caption}</p>
+      <h6 className="location">{post.location}</h6>
+      <h6 className="time">{post.time}</h6>
+      <p className="caption">{post.caption}</p>
       <img src={post.image} alt={post.caption} />
-      <h6>{post.hashtags}</h6>
+      <h6 className="hashtags">{post.hashtags}</h6>
 
-
-      <button>
-      <FontAwesomeIcon icon={faHeart} />
+      <div className="reaction-container">
+  
+  <div className="reaction-group-left">
+    <button className="reaction-button">
+      <img src={heartIcon} alt="Like" />
       <p>Like</p>
-      </button>
-      
+    </button>
+
+    <button className="reaction-button">
+      <img src={commentIcon} alt="Comment" />
+      <p>Comment</p>
+    </button>
+  </div>
+
+  
+  <div className="reaction-group-right">
+    <button className="reaction-button">
+      <img src={messageIcon} alt="Direct Message" />
+      <p>Direct Message</p>
+    </button>
+  </div>
+</div>
+
     </article>
   );
 }
