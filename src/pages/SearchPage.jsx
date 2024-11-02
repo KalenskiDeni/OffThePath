6d66c5dc1119646092452d654f773206596fd663
betaja <144784304+betaja@users.noreply.github.com>
2024-11-02 19:17:09 +0100

    fixed the searchpage issue

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,15 @@
+export default function SearchPage() {
+  return (
+    <section className="page">
+      <div className="search-bar-button">
+        <NavLink to="/search" activeClassName="active">
+          <div className="search-bar">
+            <span className="placeholder-text">
+              Tell us about your latest adventure...
+            </span>
+          </div>
+        </NavLink>
+      </div>
+    </section>
+  );
+}
