9a88fafe410bc45df0d507404157da912a111a7e
betaja <144784304+betaja@users.noreply.github.com>
2024-11-04 11:08:33 +0100

    fixed the error

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ import ChatPage from "./pages/ChatPage";
 import ProfilePage from "./pages/ProfilePage";
 import SearchPage from "./pages/SearchPage";
 import BarcelonaPage from "./pages/BarcelonaPage";
-<<<<<<< HEAD
 import UpdatePage from "./pages/UpdatePage";
-
-=======
-import FavoritesPage from "./pages/FavoritesPage";
->>>>>>> 25cd3c701f4ce1b7be2fe2b40755b24f3445b976
 
 function App() {
   return (
@@ -32,11 +27,7 @@ function App() {
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/barcelona" element={<BarcelonaPage />} />
-<<<<<<< HEAD
           <Route path="/posts/:id/update" element={<UpdatePage />} />
-=======
-          <Route path="/favorites" element={<FavoritesPage />} />
->>>>>>> 25cd3c701f4ce1b7be2fe2b40755b24f3445b976
         </Routes>
       </main>
     </>
