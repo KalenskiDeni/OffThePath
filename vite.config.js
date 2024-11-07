38bd3d10c7f014dbe2d50e7fcfd96b8ca4dbcb22
KalenskiDeni <kallenski.deni@gmail.com>
2024-11-07 13:20:41 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/OffThePath/', // Make sure this matches your repository name
   plugins: [react()],
-})
+});
