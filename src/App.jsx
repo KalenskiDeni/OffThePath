a0f9ce698d6208092a29f138f350f9f89d22aad2
Magdalena Pietras <eaa23mapi@students.eaaa.dk>
2024-11-07 12:59:30 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,9 @@ import BarcelonaPage from "./pages/BarcelonaPage";
 import UpdatePage from "./pages/UpdatePage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
-<<<<<<< Updated upstream
+
 import OnBoarding from "./pages/OnBoarding";
-=======
->>>>>>> Stashed changes
+
 import { auth } from "./firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
 import { useState } from "react";
